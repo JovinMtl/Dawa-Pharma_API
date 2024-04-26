7beca5efaea171c980ae1c4d165b746fbccf6a5c
JovinMtl <nsanzumukizath@gmail.com>
2024-04-26 11:36:22 +0200

    setting kurangura endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+#importing my models from Pharma
+from pharma.models import UmutiEntree
 
 # Create your views here.
+
+class EntrantImiti(viewsets.ViewSet):
+    """Manages all the Entrant Operations"""
+
+    @action(methods=['post'], detail=False,\
+             permission_classes= [IsAuthenticated])
+    def kurangura(self, request):
+        dataReceived = request.data
+        print(f"The data Received: {dataReceived}")
+
+        return JsonResponse({"Things ":"well"})
+
+
