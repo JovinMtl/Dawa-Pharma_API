8c2fe8bf9f0d2392f40fe6de95c62ed24f092aa2
JovinMtl <nsanzumukizath@gmail.com>
2024-04-26 11:10:31 +0200

    datefield igira datetime atari timezone

--- pharma/models.py
+++ pharma/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-from datetime import timezone
+from datetime import datetime
 
 # Create your models here.
 
 # All the structure of Pharma operations will be defined here
 
 class UmutiEntree(models.Model):
-    date_winjiriyeko = models.DateField(default=timezone.now())
-    date_uzohererako = models.DateField(default=timezone.now())
+    date_winjiriyeko = models.DateField(default=datetime.now())
+    date_uzohererako = models.DateField(default=datetime.now())
     code_umuti = models.CharField(max_length=8, default='null')  #igizwe na Lettre zitatu hamwe na chiffres zibiri
     name_umuti = models.CharField(max_length=30, default='null')
-    description_umuti = models.TextField() #ukwo bawufata n'ico umaze, bizofasha uwutawuzi
-    type_umuti = models.CharField(max_length=10, default='null')  #Flacon, plaquette, ...
+    description_umuti = models.TextField(verbose_name="ukwo bawufata n'ico umaze, bizofasha uwutawuzi")
+    type_umuti = models.CharField("Ni Flacon canke plaquette,",max_length=10, default='null')  # ...
     type_in  = models.CharField(max_length=10, default='null') #kurangura
+    ratio_type = models.FloatField(default=1) #ari carton ndayidandaza carton
     type_out = models.CharField(max_length=10, default='null') #kudetailla
     price_in = models.IntegerField(default=0)  #7: ayo Carton/plaquette yaranguwe
     price_out = models.IntegerField(default=0) #8: ayo plaquette tuyidandaza
