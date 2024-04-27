2a90976020e5fbe4a0c9275d966438c37123618b
JovinMtl <nsanzumukizath@gmail.com>
2024-04-27 20:34:19 +0200

    showing ImitiSet in the admin stie

--- pharma/admin.py
+++ pharma/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UmutiEntree
+from .models import UmutiEntree, ImitiSet
 
 # Register your models here.
 
 admin.site.register(UmutiEntree)
+admin.site.register(ImitiSet)
