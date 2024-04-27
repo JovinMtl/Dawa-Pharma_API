4a970e0562b395d75c7ebe385b539bb01c28cc7a
JovinMtl <nsanzumukizath@gmail.com>
2024-04-27 12:35:17 +0200

    making a function that will return the index of last caracter of     sous_chaine which is in a chaine

--- api/views.py
+++ api/views.py
@@ -54,6 +54,9 @@ class EntrantImiti(viewsets.ViewSet):
                     umuti_new.save()
             else:
                 #mugihe iyo code ihari muri Set
+                if ((code_set.lot)[:7] == (str(umuti.date_uzohererako))[:7]):
+                    current_lot = code_set.lot
+
                 pass
 
         print(f"The data Received: {request.user}")
@@ -81,5 +84,31 @@ class EntrantImiti(viewsets.ViewSet):
         return umuti_new
 
 # class 
+def getIndex(chaine:str, sous_chaine:str):
+    """THis one will return the index of last caracter of 
+    sous_chaine which is in a chaine"""
+
+    urufatiro = sous_chaine[0]
+    worth = True
+    
+    def nextMatch(a, b):
+        if a == b:
+            return 1
+        else:
+            return 0
+    length_chaine = len(chaine)
+    i=0
+    rep = 0
+    while worth and (i < length_chaine):
+        if urufatiro == chaine:
+            rep = nextMatch(urufatiro[i+1],chaine[i+1])
+            if(not rep):
+                worth = False
+                i += 1
+            else:
+                i +=2
+            
+
+
 
 
