4796c5db44e30eec06d2a93f909342ea13ee8e77
JovinMtl <nsanzumukizath@gmail.com>
2024-04-27 10:17:24 +0200

    modeling ImitiSet

--- pharma/models.py
+++ pharma/models.py
@@ -25,4 +25,21 @@ class UmutiEntree(models.Model):
     def __str__(self) -> str:
         return f"{self.code_umuti} {(str(self.date_winjiriyeko))[:7]}"
 
-
+class ImitiSet(models.Model):
+    code_umuti = models.CharField(max_length=8, default='null')  #igizwe na Lettre zitatu hamwe na chiffres zibiri
+    name_umuti = models.CharField(max_length=30, default='null')
+    description_umuti = models.TextField(verbose_name="ukwo bawufata n'ico umaze, bizofasha uwutawuzi")
+    type_umuti = models.CharField("Ni Flacon canke plaquette,",max_length=10, default='null')  # ...
+    type_in  = models.CharField(max_length=10, default='null') #kurangura
+    ratio_type = models.FloatField(default=1) #ari carton ndayidandaza carton
+    type_out = models.CharField(max_length=10, default='null') #kudetailla
+    price_in = models.IntegerField(default=0)  #7: ayo Carton/plaquette yaranguwe
+    price_out = models.IntegerField(default=0) #8: ayo plaquette tuyidandaza
+    difference = models.IntegerField(default=0) #9: benefice
+    quantite_restant = models.IntegerField(default=0) #10: plaquette zisigaye
+    location = models.CharField(max_length=10, default='null')  #11: ni nka cote yaho wowusanga vyoroshe
+    lot = models.TextField() #dates izohererako 
+    # [
+    #    {'date':"2025,04", 'qte':"3", 'code_operation':'12stM'},
+    #    {'date':"2025,04", 'qte':"3", 'code_operation':'12stM'},
+    # ]
