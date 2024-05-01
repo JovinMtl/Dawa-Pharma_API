dd6e7a5e18319144afb66b7e6aab26391b9fc13a
JovinMtl <nsanzumukizath@gmail.com>
2024-05-01 12:25:03 +0200

    extending the period life for a token generated

--- Dawa_Pharma_API/settings.py
+++ Dawa_Pharma_API/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("JWT",),
     "AUTH_HEADER_TYPES": ("Bearer",),
