b7036f3ca82ac9d8fe33c42e0b5be036c59d6684
JovinMtl <nsanzumukizath@gmail.com>
2024-05-03 21:49:34 +0200

    adding price_in for UmutiSold

--- pharma/models.py
+++ pharma/models.py
@@ -60,6 +60,7 @@ class UmutiSold(models.Model):
     quantity = models.IntegerField(default=1)
     price_out = models.IntegerField(default=0)
     price_total = models.IntegerField(default=1)
+    price_in = models.IntegerField(default=1)
     difference = models.IntegerField(default=0) #9: benefice
     code_operation_entrant = models.CharField(max_length=8, default='null') #code operation uyo muti winjiriyeko
     code_operation = models.CharField(max_length=8, default='null') #common with other sold together
