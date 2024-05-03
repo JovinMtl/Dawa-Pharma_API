87125c0a5e9c8863a3697478185fa3f6cca8ca25
JovinMtl <nsanzumukizath@gmail.com>
2024-05-03 23:08:22 +0200

    setting umutiReportSellSeriazer serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from pharma.models import ImitiSet
+from pharma.models import ImitiSet, umutiReportSell
 
 
 class ImitiSetSeriazer(serializers.ModelSerializer):
     class Meta:
         model = ImitiSet
+        fields = '__all__'
+
+class umutiReportSellSeriazer(serializers.ModelSerializer):
+    class Meta:
+        model = umutiReportSell
         fields = '__all__'
