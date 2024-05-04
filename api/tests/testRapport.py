73b411407802a6eadffd61b97ad62110fc34ba8d
JovinMtl <nsanzumukizath@gmail.com>
2024-05-04 10:34:50 +0200

    defining RapportTestCase

---
+++ api/tests/testRapport.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from django.test import TestCase
+
+
+class RapportTestCase(TestCase):
+    """I want to test Rapport viewset"""
+
+    def setUp(self) -> None:
+        pass
