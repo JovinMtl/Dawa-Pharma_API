d1cb07a551705da5f4ebe265065315de9b2d7885
JovinMtl <nsanzumukizath@gmail.com>
2024-05-09 17:06:00 +0200

    mocking an umutiSold

--- api_tests/test_Rapport.py
+++ api_tests/test_Rapport.py
@@ -22,6 +22,17 @@ class RapportTestCase(APITestCase):
     def test_Rapport(self):
         print(f"The result is : {self.request.json()}")
         self.assertAlmostEquals(self.request.status_code, 200)
+
+    def test_recordNew(self):
+        """Testing the record new. umuti_new is of type umutiReportSell"""
+        umuti_new = MagicMock
+        umuti_new.code_umuti = 1
+        umuti_new.name_umuti = "test 1 umuti"
+        umuti_new.nb_vente = 3
+        umuti_new.px_T_vente = 1500
+        umuti_new.benefice = 300
+        umuti_new.nb_rest = 7
+        umuti_new.px_T_rest = 3500
     
     def test_makeReport(self):
         umuti_sold_qs = MagicMock()
@@ -34,5 +45,6 @@ class RapportTestCase(APITestCase):
         individual_obj3.code_umuti = 3
         umuti_sold_qs.iterator.return_value = iter((individual_obj1, individual_obj2, individual_obj3))
 
-        response = self.instance_rapport._makeReport(umuti_sold_qs)
+        # response = self.instance_rapport._makeReport(umuti_sold_qs)
+        response = self.instance_rapport._makeReport([ individual_obj1])
         assert response == 200
