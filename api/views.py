50489eeebfb457a98bfbcebe682d1bf1b9bfb7d9
JovinMtl <nsanzumukizath@gmail.com>
2024-05-10 11:02:10 +0200

    not importing render

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.response import Response
