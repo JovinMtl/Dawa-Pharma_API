f663a4f89cda126ab71396666e22f5b34efdb421
JovinMtl <nsanzumukizath@gmail.com>
2024-05-10 11:11:35 +0200

    defining the majority of the testcases to be developped

---
+++ api_tests/test_EntrantImiti.py
@@ -0,0 +1,28 @@
+
+from rest_framework.test import APITestCase
+# from datetime import datetime
+from django.urls import reverse
+
+from unittest.mock import MagicMock, Mock
+
+#importing the class to be tested
+from api.views import Rapport
+
+
+class RapportTestCase(APITestCase):
+    """I want test all the operations done in EntrantImiti viewset"""
+
+    def setUp(self) -> None:
+        pass
+
+    def test_umutiMushasha(self):
+        pass
+
+    def test_StringToList(self):
+        pass
+
+    def test_findLastDate(self):
+        pass
+
+    def test_compileImitiSet(self):
+        pass
