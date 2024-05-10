52081ee8a33e806c515ef41f7d4fd2ef6f00b8ee
JovinMtl <nsanzumukizath@gmail.com>
2024-05-10 11:53:12 +0200

    the first development of test_umutiMushasha is made

--- api_tests/test_EntrantImiti.py
+++ api_tests/test_EntrantImiti.py
@@ -1,22 +1,41 @@
 
 from rest_framework.test import APITestCase
-# from datetime import datetime
+from datetime import datetime, timedelta
 from django.urls import reverse
 
 from unittest.mock import MagicMock, Mock
 
 #importing the class to be tested
-from api.views import Rapport
+from api.views import EntrantImiti
 
 
 class RapportTestCase(APITestCase):
     """I want test all the operations done in EntrantImiti viewset"""
 
     def setUp(self) -> None:
-        pass
+        self.instance = EntrantImiti()
 
     def test_umutiMushasha(self):
-        pass
+        umuti_entree = Mock()
+        umuti_entree.date_winjiriyeko = datetime.today()
+        umuti_entree.date_uzohererako = datetime.today() + \
+            timedelta(days=360)
+        umuti_entree.code_umuti = 'a23et'
+        umuti_entree.name_umuti = 'amoxi'
+        umuti_entree.description_umuti = 'kuvura malaria'
+        umuti_entree.type_umuti = 'Flacon'
+        umuti_entree.type_in = 'carton'
+        umuti_entree.ratio_type = 1
+        umuti_entree.type_out = 'plaquette'
+        umuti_entree.price_in = 1200
+        umuti_entree.price_out = 1500
+        umuti_entree.difference = 300
+        umuti_entree.quantite_initial = 8
+        umuti_entree.quantite_restant = 8
+        umuti_entree.location = 'a1'
+
+        reponse = self.instance._umutiMushasha(umuti=umuti_entree)
+        assert reponse.name_umuti == 'amoxi'
 
     def test_StringToList(self):
         pass
