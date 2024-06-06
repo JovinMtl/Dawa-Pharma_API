630f4b32df994bfa6703722f3a1fe497b45fa7f9
JovinMtl <nsanzumukizath@gmail.com>
2024-06-06 20:36:03 +0200

    managing to remove the empty space that starts

--- api/shared/listStrToList.py
+++ api/shared/listStrToList.py
@@ -4,7 +4,15 @@ def listStrToList(data:str):
     """ THis function takes a String of list and converts it into
       a real list"""
     # data = "['xt10', 'xt11', 'xt12']"
-    data1 = data.replace('[', '').replace(']','').replace("'",'')
+    data1 = data.replace('[',"").replace(']','').replace("'",",", -1)
     data2 = data1.split(',')
+    data3 = []
     # print(f"The result {data2}")
-    return data2
+    for dat in data2:
+        if len(dat) > 1:
+            # print(dat)
+            data3.append(dat)
+    return data3
+
+
+listStrToList()
