64ad27c823278226cf5efe3b6628b3fd23ec0115
JovinMtl <nsanzumukizath@gmail.com>
2024-06-07 10:20:52 +0200

    making listIntToList function

--- api/shared/listStrToList.py
+++ api/shared/listStrToList.py
@@ -1,7 +1,7 @@
 
 
 def listStrToList(data:str):
-    """ THis function takes a String of list and converts it into
+    """ THis function takes a String of list of Str and converts it into
       a real list"""
     # data = "['xt10', 'xt11', 'xt12']"
     data1 = data.replace('[',"").replace(']','').replace("'",",", -1)
@@ -15,4 +15,19 @@ def listStrToList(data:str):
     return data3
 
 
+def listIntToList(data:str):
+    """ THis function takes a String of list of Int and converts it into
+      a real list"""
+    data1 = data.replace('[',"").replace(']','').replace("'",",", -1)
+    data2 = data1.split(',')
+    data3 = []
+    for dat in data2:
+        if int(dat):
+            data3.append(int(dat))
+
+    return(data3)
+    
+
+
 # listStrToList()
+# listIntToList()
