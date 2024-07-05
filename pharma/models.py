9496039b2b0fbdb835447c7d11489d78dc5de523
JovinMtl <nsanzumukizath@gmail.com>
2024-07-05 09:24:39 +0200

    defining UmutiEntreeBackup model

--- pharma/models.py
+++ pharma/models.py
@@ -27,6 +27,27 @@ class UmutiEntree(models.Model):
     def __str__(self) -> str:
         return f"{self.code_umuti} {(str(self.date_winjiriyeko))[:7]}"
 
+class UmutiEntreeBackup(models.Model):
+    date_winjiriyeko = models.DateTimeField(default=timezone.now())
+    date_uzohererako = models.DateField(default=datetime.now())
+    code_umuti = models.CharField(max_length=8, default='null')  #igizwe na Lettre zitatu hamwe na chiffres zibiri
+    name_umuti = models.CharField(max_length=30, default='null')
+    description_umuti = models.TextField(verbose_name="ukwo bawufata n'ico umaze, bizofasha uwutawuzi")
+    type_umuti = models.CharField("Ni Flacon canke plaquette,",max_length=10, default='null')  # ...
+    type_in  = models.CharField(max_length=10, default='null') #kurangura  carton
+    ratio_type = models.FloatField(default=1) #ari carton ndayidandaza carton
+    type_out = models.CharField(max_length=10, default='null') #kudetailla
+    price_in = models.IntegerField(default=0)  #7: ayo Carton/plaquette yaranguwe
+    price_out = models.IntegerField(default=0) #8: ayo plaquette tuyidandaza
+    difference = models.IntegerField(default=0) #9: benefice
+    quantite_initial = models.IntegerField(default=0) # izinjiye ubwambere
+    quantite_restant = models.IntegerField(default=0) #10: plaquette zisigaye
+    location = models.CharField(max_length=10, default='null')  #11: ni nka cote yaho wowusanga vyoroshe
+    code_operation = models.CharField(max_length=12, default='null') #code yo kwinjiza uwo muti(miti):commune
+
+    def __str__(self) -> str:
+        return f"{self.code_umuti} {(str(self.date_winjiriyeko))[:7]}"
+
 class ImitiSet(models.Model):
     """THis one will contain the unique Umuti and its availability"""
     code_umuti = models.CharField(max_length=8, default='null')  #igizwe na Lettre zitatu hamwe na chiffres zibiri
