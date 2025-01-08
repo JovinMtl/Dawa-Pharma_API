b4b88c54cc6be9f24898f6f443cae224dce7ffe0
JovinMtl <nsanzumukizath@gmail.com>
2025-01-08 20:02:59 +0200

    created model named Client

--- pharma/models.py
+++ pharma/models.py
@@ -106,6 +106,13 @@ def getAssuranceInstance():
         new_bon.save()
     
     return new_bon
+class Client(models.Model):
+    nom_adherant = models.CharField(max_length=25, default='adhe')
+    employeur = models.CharField(max_length=25, default='empl')
+    beneficiaire = models.CharField(max_length=25, default='inconnu')
+    relation = models.CharField(max_length=10, default='lui-meme')
+    joined_on = models.DateField(default=timezone.now)
+
 
 class BonDeCommande(models.Model):
     beneficiaire = models.CharField(max_length=25, default='inconnu')
