93e4670d761da1a52020399029d3d54b6129a6d3
JovinMtl <nsanzumukizath@gmail.com>
2025-03-15 09:04:05 +0200

    making sure the inputs of superiorInput to be valid

--- api/shared/superiorInput.py
+++ api/shared/superiorInput.py
@@ -2,6 +2,11 @@
 
 
 def superiorInput(func, val_achat:int=0, val_vente:int=0)->int:
+    try:
+        a = int(val_achat)
+        b = int(val_vente)
+    except ValueError:
+        return 0
     minimum_vente = int(func.objects.all()[0].ben)
     if (val_vente > (minimum_vente * val_achat)):
         return val_vente
