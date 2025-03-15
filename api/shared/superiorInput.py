6db2fc2bdc2340ade7e25c583defd8a3978b3c28
JovinMtl <nsanzumukizath@gmail.com>
2025-03-15 08:48:15 +0200

    adding option of val_vente to superiorInput function

--- api/shared/superiorInput.py
+++ api/shared/superiorInput.py
@@ -1,6 +1,9 @@
 
 
 
-def superiorInput(func, val:int=0)->int:
+def superiorInput(func, val_achat:int=0, val_vente:int=0)->int:
     minimum_vente = int(func.objects.all()[0].minimum_vente)
-    return minimum_vente * val
+    if (val_vente > (minimum_vente * val_achat)):
+        return val_vente
+    else:
+        return minimum_vente * val_achat
