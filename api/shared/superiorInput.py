80834f7793742c48cde500aa245c656a50c973fb
JovinMtl <nsanzumukizath@gmail.com>
2025-03-15 08:55:47 +0200

    pointing to ben field instead of minimum_vente

--- api/shared/superiorInput.py
+++ api/shared/superiorInput.py
@@ -2,7 +2,7 @@
 
 
 def superiorInput(func, val_achat:int=0, val_vente:int=0)->int:
-    minimum_vente = int(func.objects.all()[0].minimum_vente)
+    minimum_vente = int(func.objects.all()[0].ben)
     if (val_vente > (minimum_vente * val_achat)):
         return val_vente
     else:
