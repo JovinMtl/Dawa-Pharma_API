d31cdb773a1d32f21a3112dd485bd364b771fb34
JovinMtl <nsanzumukizath@gmail.com>
2025-03-18 14:26:50 +0200

    minimum_vente is according to float number rather that int

--- api/shared/superiorInput.py
+++ api/shared/superiorInput.py
@@ -2,8 +2,10 @@
 
 
 def superiorInput(func, val_achat:int=0, val_vente:int=0)->int:
-    minimum_vente = int(func.objects.all()[0].ben)
-    if (val_vente > (minimum_vente * val_achat)):
+    minimum_vente = float(func.objects.all()[0].ben)
+    if val_vente == None:
+        return minimum_vente * val_achat
+    if (val_vente >= (minimum_vente * val_achat)):
         return val_vente
     else:
         return minimum_vente * val_achat
