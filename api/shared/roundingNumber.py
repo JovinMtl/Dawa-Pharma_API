c04b5fac753310ee2f9a51e45b6361d8f73ab6a1
JovinMtl <nsanzumukizath@gmail.com>
2025-03-31 16:26:24 +0200

    made sure 2000 stays 2000 unrounded.

--- api/shared/roundingNumber.py
+++ api/shared/roundingNumber.py
@@ -1,6 +1,7 @@
 
 def roundNumber(val:int=0)->int:
     principe = 1
+    product = 1
     if val <= 1000:
         principe = 100
         product = round(val / principe)
@@ -10,7 +11,12 @@ def roundNumber(val:int=0)->int:
         principe = 500
     elif val > 10000:
         principe = 1000
-    product = int(val / principe) + 1
+
+    if (int(val / principe)) == (val / principe):
+        product = val / principe
+    else:
+        product = int(val / principe) + 1
+        
     result = product * principe
 
     return result
