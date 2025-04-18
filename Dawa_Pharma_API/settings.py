89f28cf1d49c9679cd4a85984cadc84c692bbffd
JovinMtl <nsanzumukizath@gmail.com>
2025-04-18 15:31:07 +0200

    only refresh after two hours

--- Dawa_Pharma_API/settings.py
+++ Dawa_Pharma_API/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("JWT",),
     "AUTH_HEADER_TYPES": ("Bearer",),
