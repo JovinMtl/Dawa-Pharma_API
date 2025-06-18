643a66f319142810250f48e9b50b4b4e81b67256
JovinMtl <nsanzumukizath@gmail.com>
2025-06-18 14:03:13 +0200

    request_collection to return error 500 instead of 400 in case of no token.

--- api/views.py
+++ api/views.py
@@ -1262,11 +1262,11 @@ class GeneralOps(viewsets.ViewSet):
         token = request.data.get('imiti', 'done')
         token = token[1:]
         token = token[:-1]
-        if len(token) == 2:
+        if len(token) <= 2:
             return JsonResponse({
-                'response': 400,
+                'response': 500,
                 'counter': 0
-            }, status=400)
+            }, status=500)
         data_to_send = []
 
         meds = ImitiSet.objects.all()
